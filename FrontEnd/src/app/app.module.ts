082db85b2d3e7bbcc092e8f94d7f6aa8445f1d03
Rafael <rafael.castro@mi.unc.edu.ar>
2022-08-24 19:27:25 -0300

    Parte 2
    frontend en proceso

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HardskillsComponent } from './componentes/hardskills/hardskills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { AplogoComponent } from './componentes/aplogo/aplogo.component';
+import { SocialnetComponent } from './componentes/socialnet/socialnet.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardskillsComponent,
+    ProyectosComponent,
+    AplogoComponent,
+    SocialnetComponent,
+    BannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
