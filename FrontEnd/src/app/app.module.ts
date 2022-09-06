5a1f39c611d465905d37dc6155d5aaf2890bb049
Rafael <rafael.castro@mi.unc.edu.ar>
2022-09-05 22:47:32 -0300

    prueba 4
    casi ultima prueba

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule} from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
@@ -10,9 +10,11 @@ import { EducacionComponent } from './componentes/educacion/educacion.component'
 import { HardskillsComponent } from './componentes/hardskills/hardskills.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { AplogoComponent } from './componentes/aplogo/aplogo.component';
-
+import { HttpClientModule} from "@angular/common/http";
 import { BannerComponent } from './componentes/banner/banner.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,11 +28,15 @@ import { FooterComponent } from './componentes/footer/footer.component';
     AplogoComponent,
 
     BannerComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
